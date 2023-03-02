7ef8777c50c99502db847cc588b4e8435fd294c2
C.T. Bell <charlestbell@gmail.com>
2023-03-02 11:01:45 -0500

    It's working!

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import firebaseConfig from "./firebase-config.js";
 import { getAuth } from "firebase/auth";
 import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+import { getStorage } from "firebase/storage";
 
 const firebase = initializeApp(firebaseConfig);
 
@@ -11,5 +12,7 @@ export const auth = getAuth(firebase);
 export const db = getFirestore(firebase);
 
 export const functions = getFunctions(firebase);
+
+export const storage = getStorage(firebase);
 
 // connectFunctionsExmulator(functions, 'localhost', 5001); // Only engage when testing with sandbox, otherwise it breaks the app.
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// The ID of your GCS bucket
 import { v4 as uuidv4 } from "uuid";
+import { initializeApp, cert } from "firebase-admin/app";
+import { getStorage } from "firebase-admin/storage";
 
-const bucketName = "brog-trip-journal.appspot.com";
+import serviceAccount from "./brog-trip-journal-cc46ec503e1d.json" assert { type: "json" };
 
-// The full path of your file inside the GCS bucket, e.g. 'yourFile.jpg' or 'folder1/folder2/yourFile.jpg'
+initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: "brog-trip-journal.appspot.com",
+});
+
+const bucket = getStorage().bucket();
+
 const fileName = uuidv4();
-
-// Imports the Google Cloud client library
-import { Storage } from "@google-cloud/storage";
-
-// Creates a client
-const storage = new Storage();
 
 async function generateV4UploadSignedUrl() {
   // These options will allow temporary uploading of the file with outgoing
@@ -26,10 +24,7 @@ async function generateV4UploadSignedUrl() {
   };
 
   // Get a v4 signed URL for uploading file
-  const [url] = await storage
-    .bucket(bucketName)
-    .file(fileName)
-    .getSignedUrl(options);
+  const [url] = await bucket.file(fileName).getSignedUrl(options);
 
   console.log("Generated PUT signed URL:");
   console.log(url);
